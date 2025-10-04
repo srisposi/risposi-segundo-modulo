78e7c9c4d7328f564f402e86d5a7160de57cd7c2
sdrisposi@gmail.com <sdrisposi@gmail.com>
2025-10-04 17:02:39 -0300

    Se trae la data de la BBDD

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+
 # Configuración de la página
 st.set_page_config(
     page_title="Supply Chain Dashboard",
@@ -9,3 +12,11 @@ st.set_page_config(
 # Título principal
 st.title("📊 Supply Chain Analytics Dashboard")
 st.markdown("---")
+
+# Conectar a la base de datos SQLite
+@st.cache_data
+def load_data():
+    engine = create_engine("sqlite:///data/supply_chain.db")
+    query = "SELECT * FROM supply_chain"
+    df = pd.read_sql(query, engine)
+    return df
